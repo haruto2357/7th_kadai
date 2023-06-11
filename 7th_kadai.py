dc1e07d0cd6afd9c61e29314e1de99525dbb4ecf
haruto <23haru-it57@akane.waseda.jp>
2023-06-12 02:20:27 +0900

    make(input number)

--- 7th_kadai.py
+++ 7th_kadai.py
@@ -4,4 +4,9 @@ import random
 a = random.randint(0, 9)
 b = random.randint(0, 9)
 c = random.randint(0, 9)
-ans = 100*a + 10*b + c
+ans = (100 * a) + (10 * b) + c
+
+num = int(input('Input number : '))
+x = num // 100
+y = (num % 100) // 10
+z = num % 10
