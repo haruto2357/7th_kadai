0fcd16cf89139134a55f63ae0c4f0f6bc9845afb
haruto <23haru-it57@akane.waseda.jp>
2023-06-12 02:30:07 +0900

    make(while)

--- 7th_kadai.py
+++ 7th_kadai.py
@@ -6,7 +6,11 @@ b = random.randint(0, 9)
 c = random.randint(0, 9)
 ans = (100 * a) + (10 * b) + c
 
-num = int(input('Input number : '))
-x = num // 100
-y = (num % 100) // 10
-z = num % 10
+i = 0
+while i<5:
+    num = int(input('Input number : '))
+    x = num // 100
+    y = (num % 100) // 10
+    z = num % 10
+    
+    i = i + 1
